205d55dd318afcbb02243668c9d50af24085c967
kwame <jnaadjie@afrails.com>
2013-04-02 14:26:32 +0000

    contact-controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,18 @@
 class ContactsController < ApplicationController
-	
+
 	def new
 		@contact = Contact.new
 		render 'home/mail_us'
 	end
 
 	def create
-		@contact = Contact.new
+		@contact = Contact.new(params[:contact])
 		if @contact.save
-			flash[:notice] = "Thank you for choosing to work with us, we will get back to you within 48hrs"
+			redirect_to '/'
+			flash.now[:notice] = "Thank you for choosing to work with us, we will get back to you within 48hrs"
+
 		else
-			redirect_to 'mailto_us'
+			render 'new'
 			flash[:notice] = "Oooops looks like something went wrong, don't panic we are on it"
 		end
 	end
