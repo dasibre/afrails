8ce7db6711b498f447b56103d46efbe809a6b928
dasibre <james.naadjie@gmail.com>
2013-05-18 19:48:30 -0400

    0518cosmetic update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,17 @@ module ApplicationHelper
 	  
 	def display_notice
   		flash.each do |key,value|
-  		content_tag(:div, :class => "alert")
-  	end
-  end
+  			content_tag(:div, :class => "alert")
+    	end
+    end
+
+    def full_title(page_title)
+    	base_title = "Afrails Web design and development Services"
+    	if page_title.empty?
+    		base_title
+    	else
+    		"#{base_title} | #{page_title}"
+    	end
+    end
+
 end
