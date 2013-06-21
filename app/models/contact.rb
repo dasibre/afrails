c0a5c4875c14355215da6e0d6c4258bd140cc6a8
kwame <jnaadjie@afrails.com>
2013-06-21 15:11:19 +0000

    phone validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,10 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :message, :name, :phone
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
-  validates :name, :phone, :message, :presence => { :message  => "*This field is required" }
+  VALID_PHONE_REGEX = /[0-9]{3}-[0-9]{3}-[0-9]{4}/i
+  validates :name, :message, :presence => { :message  => "*This field is required" }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {  case_sensitive: false }
+  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX, :message => "Phone format xxx-xxx-xxxx" }
   
   def self.find_all_ordered
       find(:all, :order => 'Name, email, message')
